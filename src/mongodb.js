67e3d946ad66edf6a54443480ef155984d854883
Samrat <samrat11022002@gmail.com>
2024-05-28 02:03:39 +0530

    complete  public/home.css, public/login.css, public/signup.css, src/mongodb.js, templates/home.hbs, templates/login.hbs, templates/signup.hbs

---
+++ src/mongodb.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+
+
+const LoginSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const collection = mongoose.model("Collection", LoginSchema);
+
+module.exports = collection;
